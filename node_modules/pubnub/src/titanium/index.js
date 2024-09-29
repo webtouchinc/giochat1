6ff32d5529f11a842c6d561a082b409e660797b0
larry giovanni <touanilarryg@gmail.com>
2024-09-29 02:57:35 +0100

    Updated dependencies to address vulnerabilities

--- node_modules/pubnub/src/titanium/index.js
+++ node_modules/pubnub/src/titanium/index.js
@@ -1,16 +1,19 @@
 /* @flow */
 
+import CborReader from 'cbor-sync';
 import PubNubCore from '../core/pubnub-common';
 import Networking from '../networking';
 import Database from '../db/common';
-import { del, get, post } from '../networking/modules/titanium';
+import Cbor from '../cbor/common';
+import { del, get, post, patch } from '../networking/modules/titanium';
 import { InternalSetupStruct } from '../core/flow_interfaces';
 
 class PubNub extends PubNubCore {
   constructor(setup: InternalSetupStruct) {
     setup.db = new Database();
+    setup.cbor = new Cbor(CborReader.decode, (base64String) => new Buffer.from(base64String, 'base64'));
     setup.sdkFamily = 'TitaniumSDK';
-    setup.networking = new Networking({ del, get, post });
+    setup.networking = new Networking({ del, get, post, patch });
 
     super(setup);
   }
